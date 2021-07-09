palantir/python-language-server

Implement completionItem/resolve

pyls/hookspecs.py
@@ -28,6 +28,11 @@ def pyls_completions(config, workspace, document, position):
 
 
 @hookspec
+def pyls_completion_item_resolve(config, workspace, completion_item):
+    pass
+
+
+@hookspec
 def pyls_definitions(config, workspace, document, position):
     pass
 

pyls/plugins/jedi_completion.py
@@ -49,11 +49,17 @@ _IMPORTS = ('import_name', 'import_from')
 # Types of parso node for errors
 _ERRORS = ('error_node', )
 
+# most recently retrieved completion items, used for resolution
+_LAST_COMPLETIONS = {}
+
 
 @hookimpl
 def pyls_completions(config, document, position):
     """Get formatted completions for current code position"""
+    global _LAST_COMPLETIONS
+
     settings = config.plugin_settings('jedi_completion', document_path=document.path)
+    resolve_eagerly = settings.get('eager', False)
     code_position = _utils.position_to_jedi_linecolumn(document, position)
 
     code_position["fuzzy"] = settings.get("fuzzy", False)
@@ -79,12 +85,25 @@ def pyls_completions(config, document, position):
     if include_class_objects:
         for c in completions:
             if c.type == 'class':
-                completion_dict = _format_completion(c, False)
+                completion_dict = _format_completion(c, False, resolve=resolve_eagerly)
                 completion_dict['kind'] = lsp.CompletionItemKind.TypeParameter
                 completion_dict['label'] += ' object'
                 ready_completions.append(completion_dict)
 
+    _LAST_COMPLETIONS = {
+        # label is the only required property; here it is assumed to be unique
+        completion['label']: (completion, data)
+        for completion, data in zip(ready_completions, completions)
+    }
+
     return ready_completions or None
+
+
+@hookimpl
+def pyls_completion_item_resolve(config, completion_item):
+    """Resolve formatted completion for given non-resolved completion"""
+    completion, data = _LAST_COMPLETIONS.get(completion_item['label'])
+    return _resolve_completion(completion, data)
 
 
 def is_exception_class(name):
@@ -137,15 +156,22 @@ def use_snippets(document, position):
             not (expr_type in _ERRORS and 'import' in code))
 
 
-def _format_completion(d, include_params=True):
+def _resolve_completion(completion, d):
+    completion['detail'] = _detail(d)
+    completion['documentation'] = _utils.format_docstring(d.docstring())
+    return completion
+
+
+def _format_completion(d, include_params=True, resolve=False):
     completion = {
         'label': _label(d),
         'kind': _TYPE_MAP.get(d.type),
-        'detail': _detail(d),
-        'documentation': _utils.format_docstring(d.docstring()),
         'sortText': _sort_text(d),
         'insertText': d.name
     }
+
+    if resolve:
+        completion = _resolve_completion(completion, d)
 
     if d.type == 'path':
         path = osp.normpath(d.name)

test/plugins/test_completion.py
@@ -7,6 +7,7 @@ import pytest
 from pyls import uris, lsp
 from pyls.workspace import Document
 from pyls.plugins.jedi_completion import pyls_completions as pyls_jedi_completions
+from pyls.plugins.jedi_completion import pyls_completion_item_resolve as pyls_jedi_completion_item_resolve
 from pyls.plugins.rope_completion import pyls_completions as pyls_rope_completions
 
 
@@ -38,6 +39,10 @@ class Hello():
 print Hello().world
 
 print Hello().every
+
+def documented_hello():
+    \"\"\"Sends a polite greeting\"\"\"
+    pass
 """
 
 
@@ -60,6 +65,26 @@ def test_jedi_completion(config, workspace):
 
     # Test we don't throw with big character
     pyls_jedi_completions(config, doc, {'line': 1, 'character': 1000})
+
+
+def test_jedi_completion_item_resolve(config, workspace):
+    # Over the blank line
+    com_position = {'line': 8, 'character': 0}
+    doc = Document(DOC_URI, workspace, DOC)
+    completions = pyls_jedi_completions(config, doc, com_position)
+
+    items = {c['label']: c for c in completions}
+
+    documented_hello_item = items['documented_hello()']
+
+    assert 'documentation' not in documented_hello_item
+    assert 'detail' not in documented_hello_item
+
+    resolved_documented_hello = pyls_jedi_completion_item_resolve(
+        config,
+        completion_item=documented_hello_item
+    )
+    assert 'Sends a polite greeting' in resolved_documented_hello['documentation']
 
 
 def test_jedi_completion_with_fuzzy_enabled(config, workspace):
@@ -333,7 +358,9 @@ def test_jedi_completion_environment(workspace):
     # After 'import logh' with new environment
     completions = pyls_jedi_completions(doc._config, doc, com_position)
     assert completions[0]['label'] == 'loghub'
-    assert 'changelog generator' in completions[0]['documentation'].lower()
+
+    resolved = pyls_jedi_completion_item_resolve(completions[0]['documentation'])
+    assert 'changelog generator' in resolved['documentation'].lower()
 
 
 def test_document_path_completions(tmpdir, workspace_other_root_path):

python-lsp/python-lsp-server

pylsp/hookspecs.py
@@ -30,6 +30,11 @@ def pylsp_completions(config, workspace, document, position):
 
 
 @hookspec
+def pylsp_completion_item_resolve(config, workspace, completion_item):
+     pass
+
+
+@hookspec
 def pylsp_definitions(config, workspace, document, position):
     pass
 

pylsp/plugins/jedi_completion.py
@@ -33,11 +33,17 @@ _IMPORTS = ('import_name', 'import_from')
 # Types of parso node for errors
 _ERRORS = ('error_node', )
 
+# most recently retrieved completion items, used for resolution
+_LAST_COMPLETIONS = {}
+
 
 @hookimpl
 def pylsp_completions(config, document, position):
     """Get formatted completions for current code position"""
+    global _LAST_COMPLETIONS
+
     settings = config.plugin_settings('jedi_completion', document_path=document.path)
+    resolve_eagerly = settings.get('eager', False)
     code_position = _utils.position_to_jedi_linecolumn(document, position)
 
     code_position["fuzzy"] = settings.get("fuzzy", False)
@@ -63,12 +69,25 @@ def pylsp_completions(config, document, position):
     if include_class_objects:
         for c in completions:
             if c.type == 'class':
-                completion_dict = _format_completion(c, False)
+                completion_dict = _format_completion(c, False, resolve=resolve_eagerly)
                 completion_dict['kind'] = lsp.CompletionItemKind.TypeParameter
                 completion_dict['label'] += ' object'
                 ready_completions.append(completion_dict)
 
+    _LAST_COMPLETIONS = {
+        # label is the only required property; here it is assumed to be unique
+        completion['label']: (completion, data)
+        for completion, data in zip(ready_completions, completions)
+    }
+
     return ready_completions or None
+
+
+@hookimpl
+def pylsp_completion_item_resolve(config, completion_item):
+    """Resolve formatted completion for given non-resolved completion"""
+    completion, data = _LAST_COMPLETIONS.get(completion_item['label'])
+    return _resolve_completion(completion, data)
 
 
 def is_exception_class(name):
@@ -121,15 +140,22 @@ def use_snippets(document, position):
             not (expr_type in _ERRORS and 'import' in code))
 
 
-def _format_completion(d, include_params=True):
+def _resolve_completion(completion, d):
+    completion['detail'] = _detail(d)
+    completion['documentation'] = _utils.format_docstring(d.docstring())
+    return completion
+
+
+def _format_completion(d, include_params=True, resolve=False):
     completion = {
         'label': _label(d),
         'kind': _TYPE_MAP.get(d.type),
-        'detail': _detail(d),
-        'documentation': _utils.format_docstring(d.docstring()),
         'sortText': _sort_text(d),
         'insertText': d.name
     }
+
+    if resolve:
+        completion = _resolve_completion(completion, d)
 
     if d.type == 'path':
         path = osp.normpath(d.name)

test/plugins/test_completion.py
@@ -12,6 +12,7 @@ import pytest
 from pylsp import uris, lsp
 from pylsp.workspace import Document
 from pylsp.plugins.jedi_completion import pylsp_completions as pylsp_jedi_completions
+from pylsp.plugins.jedi_completion import pylsp_completion_item_resolve as pylsp_jedi_completion_item_resolve
 from pylsp.plugins.rope_completion import pylsp_completions as pylsp_rope_completions
 from pylsp._utils import JEDI_VERSION
 
@@ -44,6 +45,10 @@ class Hello():
 print Hello().world
 
 print Hello().every
+
+def documented_hello():
+    \"\"\"Sends a polite greeting\"\"\"
+    pass
 """
 
 
@@ -137,6 +142,26 @@ def test_jedi_completion(config, workspace):
 
     # Test we don't throw with big character
     pylsp_jedi_completions(config, doc, {'line': 1, 'character': 1000})
+
+
+def test_jedi_completion_item_resolve(config, workspace):
+    # Over the blank line
+    com_position = {'line': 8, 'character': 0}
+    doc = Document(DOC_URI, workspace, DOC)
+    completions = pylsp_jedi_completions(config, doc, com_position)
+
+    items = {c['label']: c for c in completions}
+
+    documented_hello_item = items['documented_hello()']
+
+    assert 'documentation' not in documented_hello_item
+    assert 'detail' not in documented_hello_item
+
+    resolved_documented_hello = pylsp_jedi_completion_item_resolve(
+        config,
+        completion_item=documented_hello_item
+    )
+    assert 'Sends a polite greeting' in resolved_documented_hello['documentation']
 
 
 def test_jedi_completion_with_fuzzy_enabled(config, workspace):
@@ -410,7 +435,9 @@ def test_jedi_completion_environment(workspace):
     # After 'import logh' with new environment
     completions = pylsp_jedi_completions(doc._config, doc, com_position)
     assert completions[0]['label'] == 'loghub'
-    assert 'changelog generator' in completions[0]['documentation'].lower()
+
+    resolved = pylsp_jedi_completion_item_resolve(completions[0]['documentation'])
+    assert 'changelog generator' in resolved['documentation'].lower()
 
 
 def test_document_path_completions(tmpdir, workspace_other_root_path):