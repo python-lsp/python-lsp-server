f71f4e62f4818e9f34183e2bae5da5fc73d5590c
bageljr <bageljr897@protonmail.com>
2022-04-16 21:32:27 -0500

    provide suggestions

--- pylsp/plugins/rope_autoimport.py
+++ pylsp/plugins/rope_autoimport.py
@@ -1,4 +1,6 @@
 import logging
+from collections import OrderedDict
+from typing import TypedDict
 
 from rope.contrib.autoimport import AutoImport
 
@@ -15,12 +17,47 @@ def pylsp_settings():
     return {"plugins": {"rope_autoimport": {"enabled": True}}}
 
 
+def deduplicate(input):
+    return list(OrderedDict.fromkeys(input))
+
+
 @hookimpl
 def pylsp_completions(config: Config, workspace: Workspace, document, position):
+    word = document.word_at_position(position)
+    if "." in word:
+        return []
     rope_config = config.settings(document_path=document.path).get("rope", {})
     rope_project = workspace._rope_project_builder(rope_config)
     autoimport = AutoImport(rope_project, memory=False)
+    autoimport.generate_modules_cache()
+    suggestions = deduplicate(autoimport.search_module(word))
+    suggestions.extend(deduplicate(autoimport.search_name(word)))
+    results = []
+    for import_statement, name, source, itemkind in suggestions:
+        item = {
+            "label": name,
+            "kind": itemkind,
+            "sortText": _sort_import(source, import_statement, name, word),
+            "data": {"doc_uri": document.uri},
+            "documentation": _document(import_statement),
+        }
+        results.append(item)
     autoimport.close()
+    return results
+
+
+def _document(import_statement: str) -> str:
+    return import_statement
+
+
+def _sort_import(
+    source: int, full_statement: str, suggested_name: str, desired_name
+) -> int:
+    import_length = len("import")
+    full_statement_score = 2 * (len(full_statement) - import_length)
+    suggested_name_score = 5 * (len(suggested_name) - len(desired_name))
+    source_score = 20 * source
+    return source_score + suggested_name_score + full_statement_score
 
 
 @hookimpl
---
+++ test/plugins/test_autoimport.py
@@ -0,0 +1,32 @@
+from tabulate import tabulate
+
+from pylsp import lsp, uris
+from pylsp.plugins.rope_autoimport import (
+    pylsp_completions as pylsp_autoimport_completions,
+)
+
+DOC_URI = uris.from_fs_path(__file__)
+from typing import Dict, List
+
+
+def check_dict(query: Dict, results: List[Dict]) -> bool:
+    for result in results:
+        if all(result[key] == query[key] for key in query.keys()):
+            return True
+    return False
+
+
+def test_autoimport_completion(config, workspace):
+    AUTOIMPORT_DOC = """pa"""
+    # Over 'i' in os.path.isabs(...)
+    com_position = {"line": 0, "character": 2}
+    workspace.put_document(DOC_URI, source=AUTOIMPORT_DOC)
+    doc = workspace.get_document(DOC_URI)
+    items = pylsp_autoimport_completions(config, workspace, doc, com_position)
+
+    assert items
+    print(tabulate(items))
+    assert check_dict(
+        {"label": "pathlib", "kind": lsp.CompletionItemKind.Module}, items
+    )
+    assert False
