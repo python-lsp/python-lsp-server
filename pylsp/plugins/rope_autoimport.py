e5bb74c9de02a98dacf2a659f533bf44c2a23177
bageljr <bageljr897@protonmail.com>
2022-04-18 15:58:14 -0500

    use parso to decide to use autoimport

--- pylsp/plugins/rope_autoimport.py
+++ pylsp/plugins/rope_autoimport.py
@@ -1,10 +1,13 @@
 import logging
 from collections import OrderedDict
-from typing import TypedDict
+from typing import Generator, List
 
+import parso
+from parso.python import tree
+from parso.tree import NodeOrLeaf
 from rope.contrib.autoimport import AutoImport
 
-from pylsp import hookimpl, lsp
+from pylsp import hookimpl
 from pylsp.config.config import Config
 from pylsp.workspace import Document, Workspace
 
@@ -17,21 +20,87 @@ def pylsp_settings():
     return {"plugins": {"rope_autoimport": {"enabled": True}}}
 
 
-def deduplicate(input):
+def deduplicate(input_list):
     """Remove duplicates from list."""
-    return list(OrderedDict.fromkeys(input))
+    return list(OrderedDict.fromkeys(input_list))
+
+
+def should_insert(expr: tree.BaseNode, word_node: tree.Leaf) -> bool:
+    if len(expr.children) > 0:
+        first_child = expr.children[0]
+        if isinstance(first_child, tree.EndMarker):
+            if "#" in first_child.prefix:
+                return False
+        if isinstance(
+            first_child,
+            (
+                tree.PythonLeaf,
+                tree.PythonErrorLeaf,
+            ),
+        ):
+            if first_child.value in ("import", "from") and first_child != word_node:
+                return False
+        if isinstance(first_child, (tree.PythonErrorNode)):
+            return should_insert(first_child, word_node)
+        if isinstance(first_child, tree.Keyword):
+            if first_child.value == "def":
+                return _should_import_function(word_node, expr)
+    return True
+
+
+def _should_import_function(word_node: tree.Leaf, expr: tree.BaseNode) -> bool:
+    prev_node = None
+    for node in expr.children:
+        if _handle_argument(node, word_node):
+            return True
+        if isinstance(prev_node, tree.Operator):
+            if prev_node.value == "->":
+                if node == word_node:
+                    return True
+        prev_node = node
+    return False
+
+
+def _handle_argument(node: NodeOrLeaf, word_node: tree.Leaf):
+    if isinstance(node, tree.PythonNode):
+        if node.type == "tfpdef":
+            if node.children[2] == word_node:
+                return True
+        if node.type == "parameters":
+            for parameter in node.children:
+                if _handle_argument(parameter, word_node):
+                    return True
+    return False
+
+
+def _process_statements(suggestions: List, doc_uri: str, word: str) -> Generator:
+    for import_statement, name, source, itemkind in suggestions:
+        # insert_line = autoimport.find_insertion_line(document)
+        # TODO: use isort to handle insertion line correctly
+        insert_line = 0
+        start = {"line": insert_line, "character": 0}
+        edit_range = {"start": start, "end": start}
+        edit = {"range": edit_range, "newText": import_statement + "\n"}
+        yield {
+            "label": name,
+            "kind": itemkind,
+            "sortText": _sort_import(source, import_statement, name, word),
+            "data": {"doc_uri": doc_uri},
+            "documentation": _document(import_statement),
+            "additionalTextEdits": [edit],
+        }
 
 
 @hookimpl
 def pylsp_completions(
     config: Config, workspace: Workspace, document: Document, position
 ):
-    first_word = document.word_at_position(
-        position={"line": position["line"], "character": 0}
-    )
-    word = document.word_at_position(position)
-    if first_word in ("import", "from", "#") or "." in word:
+    line = document.lines[position["line"]]
+    expr = parso.parse(line)
+    word_node = expr.get_leaf_for_position((1, position["character"]))
+    if not should_insert(expr, word_node):
         return []
+    word = word_node.value
     rope_config = config.settings(document_path=document.path).get("rope", {})
     rope_project = workspace._rope_project_builder(rope_config)
     autoimport = AutoImport(rope_project, memory=False)
@@ -39,24 +108,7 @@ def pylsp_completions(
     suggestions = deduplicate(autoimport.search_module(word))
     suggestions.extend(deduplicate(autoimport.search_name(word)))
     autoimport.close()
-    results = []
-    for import_statement, name, source, itemkind in suggestions:
-        # insert_line = autoimport.find_insertion_line(document)
-        # TODO: use isort to handle insertion line correctly
-        insert_line = 0
-        start = {"line": insert_line, "character": 0}
-        range = {"start": start, "end": start}
-        edit = {"range": range, "newText": import_statement + "\n"}
-        item = {
-            "label": name,
-            "kind": itemkind,
-            "sortText": _sort_import(source, import_statement, name, word),
-            "data": {"doc_uri": document.uri},
-            "documentation": _document(import_statement),
-            "additionalTextEdits": [edit],
-        }
-        results.append(item)
-    return results
+    return list(_process_statements(suggestions, document.uri, word))
 
 
 def _document(import_statement: str) -> str:
--- test/plugins/test_autoimport.py
+++ test/plugins/test_autoimport.py
@@ -1,10 +1,11 @@
+from typing import Dict, List
+
 from pylsp import lsp, uris
 from pylsp.plugins.rope_autoimport import _sort_import
 from pylsp.plugins.rope_autoimport import \
     pylsp_completions as pylsp_autoimport_completions
 
 DOC_URI = uris.from_fs_path(__file__)
-from typing import Dict, List
 
 
 def check_dict(query: Dict, results: List[Dict]) -> bool:
@@ -16,7 +17,6 @@ def check_dict(query: Dict, results: List[Dict]) -> bool:
 
 def test_autoimport_completion(config, workspace):
     AUTOIMPORT_DOC = """pathli"""
-    # Over 'i' in os.path.isabs(...)
     com_position = {"line": 0, "character": 6}
     workspace.put_document(DOC_URI, source=AUTOIMPORT_DOC)
     doc = workspace.get_document(DOC_URI)
@@ -29,9 +29,8 @@ def test_autoimport_completion(config, workspace):
 
 
 def test_autoimport_import(config, workspace):
-    AUTOIMPORT_DOC = """import"""
-    # Over 'i' in os.path.isabs(...)
-    com_position = {"line": 0, "character": 6}
+    AUTOIMPORT_DOC = """import """
+    com_position = {"line": 0, "character": 7}
     workspace.put_document(DOC_URI, source=AUTOIMPORT_DOC)
     doc = workspace.get_document(DOC_URI)
     items = pylsp_autoimport_completions(config, workspace, doc, com_position)
@@ -39,9 +38,50 @@ def test_autoimport_import(config, workspace):
     assert len(items) == 0
 
 
+def test_autoimport_function(config, workspace):
+    AUTOIMPORT_DOC = """def func(s"""
+    com_position = {"line": 0, "character": 10}
+    workspace.put_document(DOC_URI, source=AUTOIMPORT_DOC)
+    doc = workspace.get_document(DOC_URI)
+    items = pylsp_autoimport_completions(config, workspace, doc, com_position)
+
+    assert len(items) == 0
+
+
+def test_autoimport_function_typing(config, workspace):
+    AUTOIMPORT_DOC = """def func(s : Lis """
+    com_position = {"line": 0, "character": 16}
+    workspace.put_document(DOC_URI, source=AUTOIMPORT_DOC)
+    doc = workspace.get_document(DOC_URI)
+    items = pylsp_autoimport_completions(config, workspace, doc, com_position)
+
+    assert len(items) > 0
+
+    assert check_dict({"label": "List"}, items)
+
+def test_autoimport_function_typing_complete(config, workspace):
+    AUTOIMPORT_DOC = """def func(s : Lis ):"""
+    com_position = {"line": 0, "character": 16}
+    workspace.put_document(DOC_URI, source=AUTOIMPORT_DOC)
+    doc = workspace.get_document(DOC_URI)
+    items = pylsp_autoimport_completions(config, workspace, doc, com_position)
+
+    assert len(items) > 0
+
+    assert check_dict({"label": "List"}, items)
+
+def test_autoimport_function_typing_return(config, workspace):
+    AUTOIMPORT_DOC = """def func(s : Lis ) -> Generat:"""
+    com_position = {"line": 0, "character": 29}
+    workspace.put_document(DOC_URI, source=AUTOIMPORT_DOC)
+    doc = workspace.get_document(DOC_URI)
+    items = pylsp_autoimport_completions(config, workspace, doc, com_position)
+
+    assert len(items) > 0
+
+    assert check_dict({"label": "Generator"}, items)
 def test_autoimport_dot(config, workspace):
     AUTOIMPORT_DOC = """str."""
-    # Over 'i' in os.path.isabs(...)
     com_position = {"line": 0, "character": 4}
     workspace.put_document(DOC_URI, source=AUTOIMPORT_DOC)
     doc = workspace.get_document(DOC_URI)
@@ -52,24 +92,42 @@ def test_autoimport_dot(config, workspace):
 
 def test_autoimport_comment(config, workspace):
     AUTOIMPORT_DOC = """#"""
-    # Over 'i' in os.path.isabs(...)
-    com_position = {"line": 0, "character": 0}
+    com_position = {"line": 0, "character": 1}
     workspace.put_document(DOC_URI, source=AUTOIMPORT_DOC)
     doc = workspace.get_document(DOC_URI)
     items = pylsp_autoimport_completions(config, workspace, doc, com_position)
 
     assert len(items) == 0
+
+
+def test_autoimport_comment_indent(config, workspace):
+    AUTOIMPORT_DOC = """    # """
+    com_position = {"line": 0, "character": 5}
+    workspace.put_document(DOC_URI, source=AUTOIMPORT_DOC)
+    doc = workspace.get_document(DOC_URI)
+    items = pylsp_autoimport_completions(config, workspace, doc, com_position)
+
+    assert len(items) == 0
+
 
 def test_autoimport_from(config, workspace):
-    AUTOIMPORT_DOC = """from"""
-    # Over 'i' in os.path.isabs(...)
-    com_position = {"line": 0, "character": 6}
+    AUTOIMPORT_DOC = """from """
+    com_position = {"line": 0, "character": 5}
     workspace.put_document(DOC_URI, source=AUTOIMPORT_DOC)
     doc = workspace.get_document(DOC_URI)
     items = pylsp_autoimport_completions(config, workspace, doc, com_position)
 
-
     assert len(items) == 0
+
+
+def test_autoimport_from_(config, workspace):
+    AUTOIMPORT_DOC = """from """
+    com_position = {"line": 0, "character": 4}
+    workspace.put_document(DOC_URI, source=AUTOIMPORT_DOC)
+    doc = workspace.get_document(DOC_URI)
+    items = pylsp_autoimport_completions(config, workspace, doc, com_position)
+
+    assert len(items) > 0
 
 
 def test_sort_sources():
