9fa6b9a473f93d3a6d17e96c925c97aa3c151836
Laurent Cheylus <foxy@free.fr>
2022-06-30 12:11:35 +0200

    Fix tests for Flake8 plugin with Error severity level
    Error code Fxxx from Flake8 have now Error severity level.

--- test/plugins/test_flake8_lint.py
+++ test/plugins/test_flake8_lint.py
@@ -39,7 +39,7 @@ def test_flake8_unsaved(workspace):
     assert unused_var['code'] == 'F841'
     assert unused_var['range']['start'] == {'line': 5, 'character': 1}
     assert unused_var['range']['end'] == {'line': 5, 'character': 11}
-    assert unused_var['severity'] == lsp.DiagnosticSeverity.Warning
+    assert unused_var['severity'] == lsp.DiagnosticSeverity.Error
 
 
 def test_flake8_lint(workspace):
@@ -53,7 +53,7 @@ def test_flake8_lint(workspace):
         assert unused_var['code'] == 'F841'
         assert unused_var['range']['start'] == {'line': 5, 'character': 1}
         assert unused_var['range']['end'] == {'line': 5, 'character': 11}
-        assert unused_var['severity'] == lsp.DiagnosticSeverity.Warning
+        assert unused_var['severity'] == lsp.DiagnosticSeverity.Error
     finally:
         os.remove(name)
 
