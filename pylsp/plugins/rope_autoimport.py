1e15eb7b4acd3f5a384fadfec2af6d79ed5361a3
Tobias Krabel <tobias.krabel@databricks.com>
2023-10-17 09:49:21 +0200

    Rope autoimport - minor bug fix

--- pylsp/plugins/rope_autoimport.py
+++ pylsp/plugins/rope_autoimport.py
@@ -241,7 +241,7 @@ def pylsp_document_did_save(config: Config, workspace: Workspace, document: Docu
 
 
 @hookimpl
-def pylsp_workspace_configuration_chaged(config: Config, workspace: Workspace):
+def pylsp_workspace_configuration_changed(config: Config, workspace: Workspace):
     """
     Initialize autoimport if it has been enabled through a
     workspace/didChangeConfiguration message from the frontend.
