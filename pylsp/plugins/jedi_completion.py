edea4df65f0bdaa668780d69d80dd82f6dab6b3d
akiyuki ishikawa <aki.y.ishikawa@gmail.com>
2023-12-07 14:08:40 +0900

    Restore black line

--- pylsp/plugins/jedi_completion.py
+++ pylsp/plugins/jedi_completion.py
@@ -45,6 +45,7 @@ def pylsp_completions(config, document, position):
 
     code_position["fuzzy"] = settings.get("fuzzy", False)
     completions = document.jedi_script(use_document_path=True).complete(**code_position)
+
     if not completions:
         return None
 
