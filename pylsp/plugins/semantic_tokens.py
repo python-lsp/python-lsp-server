93e707d942dbc2fbaf41f0bbda6e1a3901c0c066
doeke <doekebuursam@gmail.com>
2024-03-10 00:16:47 -0500

    Change a few names

--- pylsp/plugins/semantic_tokens.py
+++ pylsp/plugins/semantic_tokens.py
@@ -19,7 +19,7 @@ import logging
 
 from jedi.api.classes import Name
 
-from pylsp import _utils, hookimpl
+from pylsp import hookimpl
 from pylsp.config.config import Config
 from pylsp.lsp import SemanticTokenModifier, SemanticTokenType
 from pylsp.workspace import Document
@@ -35,50 +35,48 @@ TYPE_MAP = {
     "function": SemanticTokenType.Function.value.value,
     "param": SemanticTokenType.Parameter.value.value,
     # "path": SemanticTokenType.Type.value.value,
-    # "keyword": SemanticTokenType.Type.value.value,
+    "keyword": SemanticTokenType.Keyword.value.value,
     "property": SemanticTokenType.Property.value.value,
-    # "statement": SemanticTokenType.Type.value.value,
+    # "statement": SemanticTokenType.Variable.value.value,
 }
 
 
-def _semantic_token(d: Name) -> tuple[int, int, int, int, int] | None:
-    parent_defs = d.goto(
+def _semantic_token(n: Name) -> tuple[int, int, int, int, int] | None:
+    definitions = n.goto(
         follow_imports=True,
         follow_builtin_imports=True,
         only_stubs=False,
         prefer_stubs=False,
     )
-    if not parent_defs:
-        log.info("DOEKE! NO PARENT DEFS")
+    if not definitions:
+        log.info("DOEKE! no definitions")
         return None
-    if len(parent_defs) > 1:
-        log.info("DOEKE! MORE THAN ONE PARENT DEF")
-    parent_def, *_ = parent_defs
-    log.info("DOEKE! PARENT DEF DESCRIPTION %s", parent_def.description)
-    log.info("DOEKE! PARENT DEF TYPE %s", parent_def.type)
-    if (parent_type := TYPE_MAP.get(parent_def.type, None)) is None:
+    if len(definitions) > 1:
+        log.info("DOEKE! more than one definition")
+    definition, *_ = definitions
+    log.info("DOEKE! definition type: %s", definition.type)
+    if (definition_type := TYPE_MAP.get(definition.type, None)) is None:
         return None
     # if d.name == "self":
-    #     modifier = 2**SemanticTokenModifier.Readonly.value.value
+    #     modifier = (
+    #         2**SemanticTokenModifier.Readonly.value.value
+    #         + 2**SemanticTokenModifier.DefaultLibrary.value.value
+    #     )
     # else:
     #     modifier = 0
-    return (d.line - 1, d.column, len(d.name), parent_type, 0)
+    return (n.line - 1, n.column, len(n.name), definition_type, 0)
 
 
 @hookimpl
 def pylsp_semantic_tokens(config: Config, document: Document):
-    log.info("DOEKE! semantic_tokens.pylsp_semantic_tokens")
-    log.info("DOEKE! %s %s", type(config), type(document))
-
     symbols_settings = config.plugin_settings("semantic_tokens")
-    definitions = document.jedi_names(
-        all_scopes=True, definitions=True, references=True
-    )
+    names = document.jedi_names(all_scopes=True, definitions=True, references=True)
     data = []
     line, start_char = 0, 0
-    for d in definitions:
-        log.info("DOEKE! %s (%s) %s:%s", d.description, d.type, d.line, d.column)
-        raw_token = _semantic_token(d)
+    for n in names:
+        log.info("DOEKE! name: %s, (%s:%s)", n.name, n.line, n.column)
+        log.info("DOEKE! type: %s", n.type)
+        raw_token = _semantic_token(n)
         # log.info("DOEKE! raw token %s", raw_token)
         if raw_token is None:
             continue
