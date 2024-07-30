eae92ac0bcffb365ae6e651747ad9c2dd4aef2b0
Toni <toni@codegen.com>
2024-07-30 18:47:54 +0200

    reverting changes

--- pylsp/__init__.py
+++ pylsp/__init__.py
@@ -2,16 +2,11 @@
 # Copyright 2021- Python Language Server Contributors.
 
 import os
+
 import pluggy
 
-PYLSP = "pylsp"
-IS_WIN = os.name == "nt"
-
-hookspec = pluggy.HookspecMarker(PYLSP)
-hookimpl = pluggy.HookimplMarker(PYLSP)
-
 from . import _version
-__version__ = _version.__version__
+from ._version import __version__
 
 
 def convert_version_info(version: str) -> (int, ..., str):
@@ -28,5 +23,11 @@ def convert_version_info(version: str) -> (int, ..., str):
 
 
 _version.VERSION_INFO = convert_version_info(__version__)
+
+PYLSP = "pylsp"
+IS_WIN = os.name == "nt"
+
+hookspec = pluggy.HookspecMarker(PYLSP)
+hookimpl = pluggy.HookimplMarker(PYLSP)
 
 __all__ = ("__version__",)
