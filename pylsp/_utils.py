15fbbcb1417088c861a390ee5fc9d127a99425ea
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2025-09-08 12:44:41 -0300

    fix: add missing imports for utils

--- pylsp/_utils.py
+++ pylsp/_utils.py
@@ -11,7 +11,7 @@ import subprocess
 import sys
 import threading
 import time
-from typing import Optional
+from typing import Any, Iterable, List, Optional
 
 import docstring_to_markdown
 import jedi
